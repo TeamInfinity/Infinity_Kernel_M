e848dc1ac4470ef3be2e5b2b0a500cc07ede0252
Malpa73 <kmktoczek@gmail.com>
2018-05-06 10:50:27 +0200

    improve sound v2 <3

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -104,13 +104,13 @@ enum {
 #define MICBIAS_MIN_VAL 1600000
 #define MICBIAS_STEP_SIZE 50000
 
-#define DEFAULT_BOOST_VOLTAGE 5400
+#define DEFAULT_BOOST_VOLTAGE 8000
 #define MIN_BOOST_VOLTAGE 4400
-#define MAX_BOOST_VOLTAGE 6000
+#define MAX_BOOST_VOLTAGE 8000
 #define BOOST_VOLTAGE_STEP 50
 
-#define MSM8X16_WCD_MBHC_BTN_COARSE_ADJ  60 /* in mV */
-#define MSM8X16_WCD_MBHC_BTN_FINE_ADJ 10 /* in mV */
+#define MSM8X16_WCD_MBHC_BTN_COARSE_ADJ  130 /* in mV */
+#define MSM8X16_WCD_MBHC_BTN_FINE_ADJ 15 /* in mV */
 
 #define VOLTAGE_CONVERTER(value, min_value, step_size)\
 	((value - min_value)/step_size)
@@ -1016,7 +1016,7 @@ static const struct wcd_mbhc_cb mbhc_cb = {
 
 static const uint32_t wcd_imped_val[] = {4, 8, 12, 13, 16,
 					20, 24, 28, 32,
-					36, 40, 44, 48};
+					36, 40, 44, 48, 56, 64, 72, 88, 96, 100};
 
 void msm8x16_notifier_call(struct snd_soc_codec *codec,
 				  const enum wcd_notify_event event)
@@ -1464,10 +1464,10 @@ static void msm8x16_wcd_boost_off(struct snd_soc_codec *codec)
 {
 	snd_soc_update_bits(codec,
 		MSM8X16_WCD_A_ANALOG_BOOST_EN_CTL,
-		0xDF, 0x5F);
+		0xDF, 0x80);
 	snd_soc_update_bits(codec,
 		MSM8X16_WCD_A_DIGITAL_CDC_DIG_CLK_CTL,
-		0x20, 0x00);
+		0x20, 0x20);
 }
 
 static void msm8x16_wcd_bypass_on(struct snd_soc_codec *codec)
@@ -5764,7 +5764,7 @@ static void msm8x16_wcd_set_boost_v(struct snd_soc_codec *codec)
 				snd_soc_codec_get_drvdata(codec);
 
 	snd_soc_update_bits(codec, MSM8X16_WCD_A_ANALOG_OUTPUT_VOLTAGE,
-			0x1F, msm8x16_wcd_priv->boost_voltage);
+			0x3C, msm8x16_wcd_priv->boost_voltage);
 }
 
 static void msm8x16_wcd_configure_cap(struct snd_soc_codec *codec,
@@ -5804,6 +5804,26 @@ static void msm8x16_wcd_configure_cap(struct snd_soc_codec *codec,
 #ifdef CONFIG_SOUND_CONTROL
 static struct snd_soc_codec *sound_control_codec_ptr;
 
+static ssize_t headphone_gain_store(struct kobject *kobj,
+		struct kobj_attribute *attr, const char *buf, size_t count)
+{
+
+	int input_l = 3, input_r = 3;
+
+	sscanf(buf, "%d %d", &input_l, &input_r);
+
+	if (input_l < -84 || input_l > 25)
+		input_l = 3;
+
+	if (input_r < -84 || input_r > 25)
+		input_r = 3;
+
+	snd_soc_write(sound_control_codec_ptr, MSM8X16_WCD_A_CDC_RX1_VOL_CTL_B2_CTL, input_l);
+	snd_soc_write(sound_control_codec_ptr, MSM8X16_WCD_A_CDC_RX2_VOL_CTL_B2_CTL, input_r);
+
+	return count;
+}
+
 static ssize_t headphone_gain_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
@@ -5811,26 +5831,6 @@ static ssize_t headphone_gain_show(struct kobject *kobj,
 		snd_soc_read(sound_control_codec_ptr, MSM8X16_WCD_A_CDC_RX1_VOL_CTL_B2_CTL),
 		snd_soc_read(sound_control_codec_ptr, MSM8X16_WCD_A_CDC_RX2_VOL_CTL_B2_CTL)
 	);
-}
-
-static ssize_t headphone_gain_store(struct kobject *kobj,
-		struct kobj_attribute *attr, const char *buf, size_t count)
-{
-
-	int input_l = 2, input_r = 2;
-
-	sscanf(buf, "%d %d", &input_l, &input_r);
-
-	if (input_l < -84 || input_l > 20)
-		input_l = 0;
-
-	if (input_r < -84 || input_r > 20)
-		input_r = 0;
-
-	snd_soc_write(sound_control_codec_ptr, MSM8X16_WCD_A_CDC_RX1_VOL_CTL_B2_CTL, input_l);
-	snd_soc_write(sound_control_codec_ptr, MSM8X16_WCD_A_CDC_RX2_VOL_CTL_B2_CTL, input_r);
-
-	return count;
 }
 
 static struct kobj_attribute headphone_gain_attribute =
